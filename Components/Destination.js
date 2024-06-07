5950e32ee98b11a17f01db5f02626f306fe59fec
MChurra <mchurra@outlook.pt>
2024-06-07 14:57:18 +0100

    updates

--- Components/Destination.js
+++ Components/Destination.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Data from "../data.js"
+import Data from "../public/data.js"
 
 export default function Destinations() {
     let destination = Data.map(function (dest) {
